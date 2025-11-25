77f36b5ee858a762fc5ba323cbd734f9c880262d
Ahmed Hasseiba <hasseiba.hasseiba90@gmail.com>
2025-11-25 18:21:16 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const BooklyApp2());
+}
+
+class BooklyApp2 extends StatelessWidget {
+  const BooklyApp2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
